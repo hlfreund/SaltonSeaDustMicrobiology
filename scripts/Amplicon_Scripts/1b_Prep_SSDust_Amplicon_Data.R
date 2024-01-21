7a4e138b38244f985d338124e09006196685bb20
Linton Freund <hannahfreund3@gmail.com>
2024-01-20 21:39:50 -0800

    created better color palette for the samp dates, oh and also DROPPED SB AND RHB SAMPLES because of lack of comparable sample sizes and inability to combine them into 1 site

--- scripts/Amplicon_Scripts/1b_Prep_SSDust_Amplicon_Data.R
+++ scripts/Amplicon_Scripts/1b_Prep_SSDust_Amplicon_Data.R
@@ -41,10 +41,28 @@ suppressPackageStartupMessages({ # load packages quietly
 #### Import and Prepare Data for Analyses ####
 
 ## Import ALL env plate bacterial ASV count data
-bac.ASV_table<-as.data.frame(readRDS("data/Amplicon/SaltonSeaDust_16S.V3V4_ASVTable_Robject.rds", refhook = NULL))
-dim(bac.ASV_table)
-bac.ASV_table[1:5,1:5]
+bac.ASV_table_og<-as.data.frame(readRDS("data/Amplicon/SaltonSeaDust_16S.V3V4_ASVTable_Robject.rds", refhook = NULL))
+dim(bac.ASV_table_og)
+bac.ASV_table_og[1:5,1:5]
 #bac.ASV_table<-bac.ASV_table[, !duplicated(colnames(bac.ASV_table))] # remove col duplicates
+
+# drop SB & RHB samples due to not having sample sample sizes as other sites
+#define patterns to search for
+dropped_samps <- c('SB', 'RHB')
+
+# use patterns separated by | to remove from SampleID column
+bac.ASV_table=bac.ASV_table_og[!grepl(paste(dropped_samps, collapse='|'),bac.ASV_table_og$SampleID),]
+
+# create separate ASV table to see if we are dropping all the samples we need to drop
+dropped_samps_ASVs=bac.ASV_table_og[grepl(paste(dropped_samps, collapse='|'),bac.ASV_table_og$SampleID),]
+
+# compare dimensions of original ASV table, new ASV table with unwanted samples dropped, and separate ASV table containing only dropped samples
+dim(bac.ASV_table_og) # original ASV table with all samples
+dim(bac.ASV_table) # new ASV table with unwanted samples excluded
+dim(dropped_samps_ASVs) # table of only unwanted samples
+
+# one final sanity check to see if unwanted samples are in new ASV table
+dropped_samps_ASVs$SampleID %in% bac.ASV_table$SampleID
 
 ## Import ASV taxonomic data
 bac.ASV_tax<-data.frame(readRDS("data/Amplicon/EnvMiSeq_W23_16S.V3V4_ASVs_Taxonomy_dada2_Clean_Robject.rds", refhook = NULL))
@@ -89,7 +107,7 @@ head(dust_meta)
 
 # create more specific seasons palette
 unique(dust_meta$Season_Specific)
-colorset4 = melt(c(Early.Summer="#4cc9f0",Late.Summer="#5e60ce",Early.Fall="#e85d04",Late.Fall="#9a031e",Fall.Winter="#63003a"))
+colorset4 = melt(c(Early.Summer="#4cc9f0",Late.Summer="#5e60ce",Late.Fall="#e85d04",Fall.Winter="#63003a"))
 
 colorset4$Season_Specific<-rownames(colorset4)
 colnames(colorset4)[which(names(colorset4) == "value")] <- "SeasonSpec_Color"
@@ -120,8 +138,9 @@ dust_meta$CollectionYear<-factor(dust_meta$CollectionYear, levels=c("2020","2021
 head(dust_meta)
 
 # create site palette
-unique(dust_meta$Site)
-colorset6 = melt(c("BDC"="#390099","DP"="#ffbd00","PD"="#eb5e28","RHB"="#a11d33","SB"="#0077b6","WI"="#008000"))
+unique(dust_meta$Site) # dropped SB, RHB
+# "RHB"="#a11d33","SB"="#0077b6"
+colorset6 = melt(c("BDC"="#390099","DP"="#ffbd00","PD"="#eb5e28","WI"="#008000"))
 
 colorset6$Site<-rownames(colorset6)
 colnames(colorset6)[which(names(colorset6) == "value")] <- "Site_Color"
@@ -131,13 +150,14 @@ dust_meta<-merge(dust_meta, colorset6, by="Site")
 head(dust_meta)
 dust_meta$Site_Color <- as.character(dust_meta$Site_Color)
 rownames(dust_meta)<-dust_meta$SampleID
-dust_meta$Site<-factor(dust_meta$Site, levels=c("PD","BDC","DP","WI","SB","RHB"))
+dust_meta$Site<-factor(dust_meta$Site, levels=c("PD","BDC","DP","WI"))
 
 head(dust_meta)
 
 # create collection palette
 unique(dust_meta$Seas_Coll_Year)
-colorset7 = melt(c("S.1.2020"="#14c9cb","S.2.2020"="#2962ff","S.3.2020"="#9500ff","F.1.2020"="#ff0059","S.1.2021"="#ff8c00","S.2.2021"="#0B6623","F.1.2021"="#ffd500"))
+colorset7 = melt(c("S.1.2020"="#14c9cb","S.2.2020"="#2962ff","S.3.2020"="#9500ff","F.1.2020"="#ff0059",
+                   "S.1.2021"="#ff8c00","S.2.2021"="#0B6623","F.1.2021"="#ffd500"))
 colorset7$Seas_Coll_Year<-rownames(colorset7)
 colnames(colorset7)[which(names(colorset7) == "value")] <- "SCY_Color"
 colorset7
@@ -154,17 +174,25 @@ head(dust_meta)
 dust_meta$SampDate<-interaction(dust_meta$SampleMonth,dust_meta$CollectionYear,sep=".")
 head(dust_meta)
 unique(dust_meta$SampDate)
+length(unique(dust_meta$SampDate)) # 8 different sampdates
 dust_meta$SampDate<-factor(dust_meta$SampDate, levels=c("July.2020","August.2020","October.2020","November.2020",
                                                         "July.2021","August.2021","September.2021","December.2021"))
 
 # check brewer palette for 8 different colors for SampDate variable, then create colorset8 for SampDate
-display.brewer.pal(n = 8, name = 'Dark2')
-colorset8=data.frame(SampDate_Color=(brewer.pal(n = 8, name = "Dark2")))
-rownames(colorset8)<-unique(dust_meta$SampDate[order(dust_meta$SampDate)])
-colorset8
-colorset8$SampDate<-rownames(colorset8)
+# display.brewer.pal(n = 8, name = 'Dark2')
+# colorset8=data.frame(SampDate_Color=(brewer.pal(n = 8, name = "Dark2")))
+# rownames(colorset8)<-unique(dust_meta$SampDate[order(dust_meta$SampDate)])
+# colorset8
+# colorset8$SampDate<-rownames(colorset8)
 
-# merge metadata and colors together
+# create collection palette
+unique(dust_meta$SampDate)
+colorset8 = melt(c("July.2020"="green2","August.2020"="orange","October.2020"="red","November.2020"="purple",
+                   "July.2021"="darkgreen","August.2021"="darkorange3","September.2021"="red4","December.2021"="purple4"))
+colorset8$SampDate<-rownames(colorset8)
+colnames(colorset8)[which(names(colorset8) == "value")] <- "SampDate_Color"
+colorset8
+
 dust_meta<-merge(dust_meta, colorset8, by="SampDate")
 head(dust_meta)
 dust_meta$SampDate_Color <- as.character(dust_meta$SampDate_Color)
@@ -172,6 +200,11 @@ rownames(dust_meta)<-dust_meta$SampleID
 
 # check if SampDate still has its factor levels
 unique(dust_meta$SampDate)
+
+# dust_meta$SampDate<-factor(dust_meta$SampDate, levels=c("July.2020","August.2020","October.2020","November.2020",
+#                                                         "July.2021","August.2021","September.2021","December.2021"))
+
+
 
 #### Import Surface Type Frequencies Data ####
 # data from Will Porter's group
@@ -185,6 +218,11 @@ SurfTypFreq<-as.data.frame(read.csv("data/Climate/PorterLab_SSD_SurfaceTypeFrequ
 head(SurfTypFreq)
 rownames(SurfTypFreq)<-SurfTypFreq$SampleID
 head(SurfTypFreq) #sanity check
+
+unique(SurfTypFreq$STF_Date)
+SurfTypFreq$STF_Date<-factor(SurfTypFreq$STF_Date, levels=c("July.2020","August.2020","October.2020","November.2020",
+                                                        "August.2021","October.2021","December.2021"))
+
 
 # back.traj<-read.fst("data/SaltonSeaDust_PorterLab_TrajectoryData.fst")
 # colnames(back.traj)[which(names(back.traj) == "site")] <- "Site"
@@ -206,16 +244,17 @@ head(SurfTypFreq) #sanity check
 bac.ASV_table[1:5,1:5]
 bac.ASV_tax[1:5,1:5]
 
-bac.ASV_t.table<-as.data.frame(t(bac.ASV_table[,-1]))
+bac.ASV_t.table<-as.data.frame(t(bac.ASV_table[,-1])) # transpose ASV table
 bac.ASV_t.table[1:5,1:5]
 bac.ASV_t.table$ASV_ID<-rownames(bac.ASV_t.table)
 bac.ASV_t.table[1:5,5-ncol(bac.ASV_t.table):ncol(bac.ASV_t.table)]
 
-bac.ASV_all<-merge(bac.ASV_t.table,bac.ASV_tax, by="ASV_ID")
+bac.ASV_all<-merge(bac.ASV_t.table,bac.ASV_tax, by="ASV_ID") # merge ASV table & taxa table by ASV_IDs
 head(bac.ASV_all)
 dim(bac.ASV_all)
 #bac.ASV_all<-bac.ASV_all[, !duplicated(colnames(bac.ASV_all))] # remove col duplicates
 
+# melt combined ASV + taxa table to then merge with metadata by SampleID
 bac.dat.dust<-melt(bac.ASV_all)
 head(bac.dat.dust)
 colnames(bac.dat.dust)[which(names(bac.dat.dust) == "variable")] <- "SampleID"
@@ -230,6 +269,8 @@ dim(bac.ASV_table[rownames(bac.ASV_table) %in% rownames(dust_meta),])
 
 # update ASV table
 bac.ASV_table<-bac.ASV_table[rownames(bac.ASV_table) %in% rownames(dust_meta),] # drop technical reps of samples
+dim(bac.ASV_table)
+bac.ASV_table[1:6,1:6]
 
 #### Drop Rep Letter from Sample Names ####
 
@@ -248,7 +289,6 @@ b.dust.all$SampleID<-gsub("(.*\\..*)\\..*","\\1", b.dust.all$SampleID)
 b.dust.all$SampleID # sanity check
 head(b.dust.all)
 
-
 # check if sampleIDs match between metadata & ASV table
 rownames(dust_meta) %in% rownames(bac.ASV_table)
 
@@ -259,8 +299,11 @@ dust_meta=dust_meta[rownames(bac.ASV_table),] ## will drop rows that are not sha
 
 dust.meta.surf<-merge(dust_meta,SurfTypFreq,by=c("Site","SampleID"))
 head(dust.meta.surf)
+dim(dust.meta.surf)
 
-#### Check Read Distribution Across Samples ####
+rownames(dust.meta.surf)<-dust.meta.surf$SampleID
+
+#### Check ASV Count Distribution Across Samples ####
 rowSums(bac.ASV_table[,-1])
 
 #### Save Global Env for Import into Other Scripts ####
